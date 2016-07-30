a1059d65909b1040788e3650137d4d9ab5c57523
Rehan Ahmed <Rehan.ahmed19@outlook.com>
2016-07-30 20:29:50 +0500

    Add sticky navbar and add media queries

--- js/script.js
+++ js/script.js
@@ -23,5 +23,16 @@ $(document).ready(function(){
 	  });
 	});
 
+	/* STICKY NAVBAR */
+	$('.about').waypoint(function(direction){
+		if (direction == "down") {
+			$('nav').addClass("sticky");
+		}else{
+			$('nav').removeClass("sticky");
+		}
+	}, {
+		offset: "60px"
+	});
+
 
 });
